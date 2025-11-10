55326e1686483101dd10c24783f3553d79cddf9b
greenWave <hlakhal@etu.uqac.ca>
2025-11-09 22:01:44 -0500

    feat: middlewares + endpoint santé + compose env_file

--- server.js
+++ server.js
@@ -1,7 +1,15 @@
 import express from "express";
+import cors from "cors";
+import morgan from "morgan";
+
 const app = express();
-app.get("/", (req, res) => res.send("Hello Docker + Git 👋"));
+app.use(cors());
+app.use(express.json());
+app.use(morgan("dev"));
+
+app.get("/", (_req, res) => res.send("Hello Docker + Git 👋"));
+app.get("/api/health", (_req, res) => res.json({status:"ok", uptime:process.uptime()}));
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`http://localhost:${PORT}`));
-app.get("/api/health", (_req, res) => res.json({status:"ok", uptime:process.uptime()}));
 
